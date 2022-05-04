6841f705d79aa57c4379dd9b4a253e2cb460546e
mk21r <mk21red@gmail.com>
2022-05-04 10:08:41 +0100

    ScotlandYard

--- ScotlandYard/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ ScotlandYard/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,11 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * cw-model
@@ -16,6 +20,69 @@ public final class MyModelFactory implements Factory<Model> {
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
+		class Mymodel implements Model {
+			private Board.GameState MyGame;
+			private Set<Observer> Observers;
+			private Mymodel() {
+				this.MyGame = new MyGameStateFactory().build(setup, mrX, detectives);
+				this.Observers = new HashSet<>();
+			}
+
+			@Nonnull
+			@Override
+			public Board getCurrentBoard() {
+				return MyGame;
+			}
+
+			@Override
+			public void registerObserver(@Nonnull Observer observer) {
+				if(Observers.contains(observer))throw new IllegalArgumentException("duplicate observer");
+				if(observer.equals(null))throw new IllegalArgumentException("observer cannot be null");
+
+				Observers.add(observer);
+
+			}
+
+			@Override
+			public void unregisterObserver(@Nonnull Observer observer) {
+				if(observer.equals(null))throw new IllegalArgumentException("observer cannot be null");
+				if(!Observers.contains(observer))throw new IllegalArgumentException("observer already unregistered");
+				if(Observers.isEmpty()) throw new IllegalArgumentException("observers empty");
+				Observers.remove(observer);
+
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Observer> getObservers() {
+				ImmutableSet<Observer> newObservers = ImmutableSet.copyOf(Observers);
+				return newObservers;
+			}
+
+			@Override
+			public void chooseMove(@Nonnull Move move) {
+				// TODO Advance the model with move, then notify all observers of what what just happened.
+
+				MyGame = MyGame.advance(move);
+				if(!MyGame.getWinner().isEmpty()){
+					for (Observer observer: getObservers()) {
+						observer.onModelChanged(MyGame, Observer.Event.GAME_OVER);
+					}
+
+				}
+				else{
+					for (Observer observer: getObservers()) {
+						observer.onModelChanged(MyGame, Observer.Event.MOVE_MADE);
+					}
+				}
+
+
+				//  you may want to use getWinner() to determine whether to send out Event.MOVE_MADE or Event.GAME_OVER
+
+			}
+		}
+		return new Mymodel();
 	}
+
+
 }
